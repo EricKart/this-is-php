a4cc1ab9dda78f37e793d61535c60f0da14452b3
EricKart <75946757+EricKart@users.noreply.github.com>
2021-07-08 16:19:11 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,27 @@
+<?php 
+include_once 'header.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Auth System</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.cdd">
+</head>
+<body>
+<nav>
+    <div class ="wrapper">
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="discover.php">About us</a></li>
+            <li><a href="blog.php">Find Blogs</a></li>
+            <li> <a href="SignUp.php">Sign Up</a></li>
+            <li><a href="login.php">Log in</a></li>
+        </ul>
+    </div>
+</nav>
+<div class="wrapper">
---
+++ index.php
@@ -0,0 +1,26 @@
+<?php 
+include_once 'header.php';
+?>
+
+    <section class="index.intro">
+        <h1>This is an Introduction</h1>
+        <p>Here is  the imporatant Paragraph that will explain you about the purpose of the Website</p>
+    </section>
+    <section class="index-categories">
+        <h2>Some Basic categories</h2>
+        <div class="index-categories-list">
+            <div>
+                <h3>Fun Stuff</h3>
+            </div>
+            <h3>Serious Stuff</h3>
+        </div>
+        <div>
+            <h3>Exciting Stuff</h3>
+        </div>
+        <div>
+            <h3>Boring Stuff</h3>
+        </div>
+    </section>
+<?php 
+include_once 'footer.php';
+?>
